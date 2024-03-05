0734a4cea0498c64d1f3f0056f4b169b41c9d018
bruno28araujo <bruno28araujo@gmail.com>
2024-03-05 09:38:42 -0300

    atualizado 01

---
+++ index.js
@@ -0,0 +1,11 @@
+torrando()
+
+function torrando(){
+    console.log("Preparando para injetar o pão.")
+    torrar()
+    console.log("Pão torrado1")
+}
+
+function torrar(){
+    console.log("Torrando o pão.")
+}
